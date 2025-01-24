dc17658d039b52ce2414f953b067627f890e43d4
Biswarup Chaterjee <chatterjeebiswarup61@gmail.com>
2025-01-24 13:44:07 +0530

    final commit

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import BusinessPromoters from "../pages/businessPromoters/BsinessPromoters";
 
 export default function AppRoute() {
